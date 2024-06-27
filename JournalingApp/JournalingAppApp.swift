1cf736c1a436587d11e07eef9b9ecc39088c0249
Jkim171 <jonathankim717@gmail.com>
2024-06-26 20:55:16 -0500

    Initial Commit

---
+++ JournalingApp/JournalingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingAppApp.swift
+//  JournalingApp
+//
+//  Created by Jonathan Kim on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
