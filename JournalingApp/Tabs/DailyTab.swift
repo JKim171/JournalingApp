1aa2db4493d14e54f3e7a2edad16ba11c8a9da62
Jkim171 <jonathankim717@gmail.com>
2024-07-20 13:39:28 -0500

    Small ui changes

--- JournalingApp/Tabs/DailyTab.swift
+++ JournalingApp/Tabs/DailyTab.swift
@@ -8,35 +8,36 @@
 import SwiftUI
 
 struct DailyTab: View {
+    @State var a1: String = ""
+    @State var a2: String = ""
+    @State var a3: String = ""
+    @State var mood: Float = 0
+
     var body: some View {
         NavigationStack {
-            VStack(alignment: .center, spacing: 10){
-                Text("DAILY TASKS:")
-                    .font(.largeTitle)
-                    .background(Color.blue.opacity(0.2))
+            ZStack {
+                Color.blue.ignoresSafeArea()
                 
-                @State var a1: String = ""
-                @State var a2: String = ""
-                @State var a3: String = ""
-                @State var mood: Float = 0
-                
-                askQuestion(question: "How was your day?", ans: $a1)
-                askQuestion(question: "What's been on your mind the most?", ans: $a2)
-                askQuestion(question: "Who've you talked to?", ans: $a3)
-                
-                
-                Text("Mood Rating: \(mood, specifier: "%.0f")")
-                    .padding()
-                HStack{
-                    Text("0")
-                    Slider(value: $mood, in: 0...10, step: 1)
-                    Text("10")
+                VStack(alignment: .center, spacing: 10) {
+                    Text("DAILY TASKS:")
+                        .font(.largeTitle)
+                    
+                    askQuestion(question: "How was your day?", ans: $a1)
+                    askQuestion(question: "What's been on your mind the most?", ans: $a2)
+                    askQuestion(question: "Who've you talked to?", ans: $a3)
+                    
+                    Text("Mood Rating: \(mood, specifier: "%.0f")")
+                        .padding().background(Color.white)
+                    
+                    HStack {
+                        Text("0")
+                        Slider(value: $mood, in: 0...10, step: 1)
+                        Text("10")
+                    }
                 }
-                
-                
-            }.padding()
+                .padding()
                 .toolbar {
-                    ToolbarItem{
+                    ToolbarItem {
                         Button {
                             print("Settings")
                         } label: {
@@ -44,19 +45,18 @@ struct DailyTab: View {
                         }
                     }
                 }
+            }
         }
     }
     
     func askQuestion(question: String, ans: Binding<String>) -> some View {
-        VStack{
-            //Text("\(question)")
+        VStack {
             TextField("\(question)", text: ans)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-                .background(Color.gray.opacity(0.1))
+                .foregroundColor(.black)
         }
     }
-    
 }
 
 #Preview {
